9be6c2dd13549db460d52396f6e2f60a7dd56484
Lucas.Palacios <lucaspalacios698@gmail.com>
2026-01-08 10:46:53 -0300

    ,

--- routes/test.router.js
+++ routes/test.router.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import  authMiddleware from '../middlewares/auth.middlewares.js';
 
 const testRouter = express.Router();
 
@@ -8,5 +9,21 @@ testRouter.get(
         response.send('test hecho')
     }
 );
+
+testRouter.get(
+    '/authorized-test',
+    authMiddleware,
+    (request, response) => {
+        console.log({user_data: request.user})
+        return response.json(
+            {
+                ok: true, 
+                status: 200,
+                message: 'Test correcto',
+                data: null
+            }
+        )
+    }
+)
 
 export default testRouter;
